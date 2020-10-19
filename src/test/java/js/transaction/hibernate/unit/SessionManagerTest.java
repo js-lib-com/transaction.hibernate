7b3fcfa5238bfe5922294d9f59b6f29a9036550f
Iulian Rotaru <iulian@gnotis.ro>
2020-10-19 18:55:52 +0300

    Upgrade to Java 8 and JUnit 4.13.1 .

--- src/test/java/js/transaction/hibernate/unit/SessionManagerTest.java
+++ src/test/java/js/transaction/hibernate/unit/SessionManagerTest.java
@@ -1,9 +1,9 @@
 package js.transaction.hibernate.unit;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
